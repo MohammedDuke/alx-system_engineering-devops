659545ea2c19b924de7b8f4b579bf2b6b065af9b
Mohamed Ahmed <hamoahmed8055@gmail.com>
2024-05-06 08:29:51 -0700

    fixing issue

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,19 @@
+
 #!/usr/bin/python3
-"""Module that consumes the Reddit API and returns the number of subscribers"""
-import requests
+"""Module for task 0"""
+
 
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers"""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/mohammed)"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data["data"]["subscribers"]
-        return subscribers
-    else:
+    """Queries the Reddit API and returns the number of subscribers
+    to the subreddit"""
+    import requests
+
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
         return 0
+
+    return sub_info.json().get("data").get("subscribers")
+
